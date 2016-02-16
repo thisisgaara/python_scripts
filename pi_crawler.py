095a212f734ada3cdbd34dc0a868b42a763e0811
thisisgaara <k.vigneshkannan@gmail.com>
2016-02-15 23:58:27 -0700

    Adding pi crawler

---
+++ pi_crawler.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 15 23:49:18 2016
+
+@author: Vignesh_Kannan
+Purpose: Crawler to download all the editions of Raspberry pi magazine
+Version1: Simple implementaiton to download it to disk
+Version2: Adding google drive API to upload in drive by making its own directory
+"""
+
