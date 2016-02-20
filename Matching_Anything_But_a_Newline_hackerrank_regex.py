5729f06101810258422975a4f6f3c1bb64e1f0cf
thisisgaara <k.vigneshkannan@gmail.com>
2016-02-19 23:49:19 -0700

    Adding hackerrank scripts

---
+++ Matching_Anything_But_a_Newline_hackerrank_regex.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 19 23:42:53 2016
+
+@author: ASU_CUDA_LAPTOP
+"""
+
+Regex_Pattern = r"[^\n]{3}\.[^\n]{3}\.[^\n]{3}\.[^\n]{3}"	# Do not delete 'r'.
+
+
