64574b2e11f116885e8973b57a18f9df8e5a2751
thisisgaara <k.vigneshkannan@gmail.com>
2016-02-23 01:15:25 -0700

    Finishing sed

--- Text processing Hackerrank/sed.sh
+++ Text processing Hackerrank/sed.sh
@@ -3,3 +3,4 @@ Day 5:
 'Sed' command #2 sed -e 's/[tT][Hh][Yy]/your/g'
 'Sed' command #3 sed -e 's/[tT][Hh][Yy]/{&}/g'
 'Sed' command #4 sed -e 's/[0-9]\{4\}\s/**** /1' | sed -e 's/[0-9]\{4\}\s/**** /1' | sed -e 's/[0-9]\{4\}\s/**** /1'
+Command #5 sed -r -e 's/(.... )(.... )(.... )(....)/\4 \3\2\1/g'
